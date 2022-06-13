4bb37b4af81a74075b7ec99f2de00aa0ae9ee33a
developerkimjh <developerkimjh@gmail.com>
2022-06-13 23:39:38 +0900

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { routes } from "./routes";
+import { useRoutes } from "react-router-dom";
+
+const App = () => {
+  const elem = useRoutes(routes);
+  return elem;
+};
+
+export default App;
